3637d92d9c00967346b4fecab0e623a8999e99ea
Jose Montero <josemariodev@gmail.com>
2020-01-14 12:47:13 -0600

    Complete modules through 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+fooTxt = open('foo.txt')
+print(fooTxt.read())
+fooTxt.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+barTxt = open("bar.txt", 'r+')
+barTxt.write("Line one ")
+barTxt.write("Line two ")
+barTxt.write("Line three ")
+barTxt.close()
+barTxt = open("bar.txt")
+print(barTxt.read())
+barTxt.close()
